232a13ffbed3e0bc660e5505c068a659492dd53c
hmonk <ni.sephiroth@gmai.com>
2018-04-20 12:47:52 +0200

    update

--- timer.js
+++ timer.js
@@ -9,9 +9,9 @@ for (i=1; i<120; i++){
 	hours = date.getHours();
 	minutes = "0" + date.getMinutes();
 	seconds = "0" + date.getSeconds();
+	time = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 
-	console.log("{")
-	console.log('"time":', '"' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2) + '",')
+	console.log(time , txpool.status.pending, txpool.status.queued)
 	console.log('"pending":',  txpool.status.pending, ',')
 	console.log('"queued":',  txpool.status.queued)
 	console.log("}")
