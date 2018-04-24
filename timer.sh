754e7d8d31392b27b080f8e3934d55ce8a4e1dbb
hmonk <ni.sephiroth@gmai.com>
2018-04-24 12:16:25 +0200

    Merge branch 'master' of https://github.com/hmonk/qptest

--- timer.sh
+++ timer.sh
@@ -4,10 +4,11 @@ set -e
 
 mkdir -p ~/qptest/timer
 
-filename=$(date +%Y%m%d_%H)
+
 
 
 for ((num=1; num<=24; num++)){
+    filename=$(date +%Y%m%d_%H)
     echo "start getting txpool per second, log being saved to qptest/timer/$filename.log ......"
     geth --exec 'loadScript("./qptest/timer.js")' attach ~/qdata/geth.ipc &>>./qptest/timer/$filename.log
 }
