6c86446137286d00bd0e31eed6ae5cbd125fecac
hmonk <ni.sephiroth@gmai.com>
2018-05-17 19:11:04 +0200

    65536 2B

--- geth/start.sh
+++ geth/start.sh
@@ -5,5 +5,5 @@ set -e
 sh constellation-start.sh
 set -v
 ARGS="--syncmode full --mine --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul"
-geth --datadir qdata $ARGS --rpcport 9495 --port 30303 --txpool.globalslots 16384 --metrics --targetgaslimit '200000000' --unlock 0 --password pwd.txt
+geth --datadir qdata $ARGS --rpcport 9495 --port 30303 --txpool.globalslots 65536 --metrics --targetgaslimit '2000000000' --unlock 0 --password pwd.txt
 set +v
